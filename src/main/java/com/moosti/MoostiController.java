587c5b6e4f75e25defb228bc6a8a960625c7b1ad
Trifanenko Dmitrij <dimkas71g@github.com>
2016-02-10 15:51:55 +0200

    fix problem with play method in MethodController. change mp3 library to JLayer from Javafx MediaPlayer

--- src/main/java/com/moosti/MoostiController.java
+++ src/main/java/com/moosti/MoostiController.java
@@ -6,12 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaException;
-import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
-
-import java.net.URISyntaxException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 
 
 /**
@@ -62,8 +59,8 @@ public class MoostiController {
             @Override
             public void onFinish() {
                 stopTimer();
-                play();
                 Platform.runLater(() -> text.setText(Utils.fromMillis(0)));
+                play();
             }
             @Override
             public void onTick(long millisUntilFinished) {
@@ -97,14 +94,10 @@ public class MoostiController {
     }
 
     private void play() {
-        final Media media;
         try {
-            media = new Media(getClass().getResource("ring_end.mp3").toURI().toString());
-            final MediaPlayer mp = new MediaPlayer(media);
-            mp.play();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (MediaException e) {
+            Player player = new Player(getClass().getResourceAsStream("ring_end.mp3"));
+            player.play();
+        } catch (JavaLayerException e) {
             e.printStackTrace();
         }
 
